1f7a377d998878a3e982febf220b2f9914503432
user1 <user1@DESKTOP-Q4URS8H>
2023-10-08 20:07:17 +0900

    CSRFtest

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -18,9 +18,13 @@ import { NONAME } from 'dns';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService){
+  constructor(private readonly authService: AuthService){}
 
+  @Get('/csrf')
+  getCsrfToken(@Req() req: Request):Csrf{
+    return {csrfToken: req.csrfToken()};
   }
+
   @Post('signup')
   signUp(@Body() dto: AuthDto): Promise<Msg>{
     return this.authService.signUp(dto);
@@ -35,7 +39,7 @@ export class AuthController {
     const jwt = await this.authService.login(dto);
     res.cookie('access_token',jwt.accessToken,{
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite:'none',
       path: '/',
     });
@@ -49,7 +53,7 @@ export class AuthController {
   logout(@Req() req: Request,@Res({passthrough: true}) res:Response): Msg {
     res.cookie('access_token', '', {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite:'none',
       path:'/',
     });
